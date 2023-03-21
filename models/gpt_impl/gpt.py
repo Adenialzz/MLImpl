6d06eb4dcf811bfe5c7f0f2c812f82a37f09924a
Adenialzz <1160180309@qq.com>
2023-03-21 13:50:49 +0800

    add gpt generating

--- models/gpt_impl/gpt.py
+++ models/gpt_impl/gpt.py
@@ -17,6 +17,9 @@ class LayerNorm(nn.Module):
         return F.layer_norm(x, self.weight.shape, self.weight, self.bias, 1e-5)
 
 class CausalSelfAttention(nn.Module):
+    # 如果PyTorch>=2.0，可以使用官方实现的F.scaled_dot_product_attention
+    # 基于CUDA核实现，速度更快
+    # 此处手动实现
     def __init__(self, config):
         super().__init__()
         assert config.n_embed % config.n_head == 0
@@ -30,6 +33,7 @@ class CausalSelfAttention(nn.Module):
         self.n_embed = config.n_embed
         self.dropout = config.dropout
 
+        # causal mask用于保证仅计算当前位置左侧的注意力
         self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
 
     def forward(self, x):
@@ -40,12 +44,13 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
 
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1))) # scaled
         att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))  # mask
         att = F.softmax(att, dim=-1)
         att = self.attn_dropout(att)
-        y = att @ v
-        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # 计算完成后，重装所有头的输出
         y = self.resid_dropout(self.c_proj(y))
         return y
 
@@ -102,6 +107,9 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embed)
             ))
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+        # 将输入映射矩阵`transformer.wte.weight`和输出映射矩阵`lm_head.weight`的参数绑定
+        # ref：# https://paperswithcode.com/method/weight-tying
+
         self.transformer.wte.weight = self.lm_head.weight
 
         # init weights
@@ -156,6 +164,114 @@ class GPT(nn.Module):
         self.transformer.wpe.weight = nn.Parameter(self.transformer.wpe.weight[: block_size])
         for blk in self.transformer.h:
             blk.attn.bias = blk.attn.bias[:, :, :block_size, :block_size]
+
+    @classmethod
+    def from_pretrained(cls, model_type, override_args=None):
+        assert model_type in ('gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl')
+        override_args = override_args or {}
+        # 只有dropout可以被重写
+        assert all(k == 'dropout' for k in override_args)
+        from transformers import GPT2LMHeadModel
+        print(f'loading weights from pretraiend gpt: {model_type}')
+
+        config_args = {
+                'gpt2':         dict(n_layer=12, n_head=12, n_embed=768),  # 124M Params
+                'gpt2-medium':  dict(n_layer=24, n_head=16, n_embed=1024), # 350M Params
+                'gpt2-large':   dict(n_layer=36, n_head=20, n_embed=1280), # 774M Params
+                'gpt2-xl':      dict(n_layer=48, n_head=25, n_embed=1600), # 1558M Params
+        }[model_type]
+
+        print('focing vocab_size=50257, block_size=1024, bias=True')
+        config_args['vocab_size'] = 50257
+        config_args['block_size'] = 1024
+        config_args['bias'] = True
+
+        if 'dropout' in override_args:
+            print(f"overriding dropout rate to {override_args['dropout']}")
+            config_args['dropout'] = override_args['dropout']
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')]  # 丢弃 mask/buffer，这不是参数
+
+        # 获取huggingface/transformers的权重参数
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # 复制参数，保证所有参数的名称与形状是对应的
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # 丢弃buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # 丢弃mask
+
+        # openai权重使用Conv1D，而我们的实现使用Linear，因此在导入以下权重时需要转置
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        assert len(sd_keys_hf) == len(sd_keys)
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape, \
+                print(f'transposed model params {k} shape misaligned: hf: {sd_hf[k].shape[::-1]}, model: {sd[k].shape}')
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape, \
+                print(f'model params {k} shape misaligned: hf: {sd_hf[k].shape}, model: {sd[k].shape}')
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        return model
+    def configure_optimizers(self, weight_decay, learning_rate, betas, device_type):
+        """
+        该函数用于配置GPT的优化器，之所以这么冗长是因为需要根据是否需要权重衰减将GPT的参数分为两部分
+        并配置不同的优化器
+        """
+
+        # 将所有参数根据是否需要参数衰减分为两部分，白名单需要，黑名单不需要
+        decay = set()
+        no_decay = set()
+        whitelist_weight_modules = (torch.nn.Linear, )
+        blacklist_weight_modules = (torch.nn.LayerNorm, LayerNorm, torch.nn.Embedding)
+        for mn, m in self.named_modules():
+            for pn, p in m.named_parameters():
+                fpn = '%s.%s' % (mn, pn) if mn else pn # 参数全名
+                # 由于`named_modules`和`named_parameters`是递归的，
+                # 因此我们会多次看到同一个张量p，
+                # 这样做我们可以知道张量p来自哪个父模块
+                if pn.endswith('bias'):
+                    # 所有bias都需要权重衰减
+                    no_decay.add(fpn)
+                elif pn.endswith('weight') and isinstance(m, whitelist_weight_modules):
+                    # 白名单中的参数需要权重衰减
+                    decay.add(fpn)
+                elif pn.endswith('weight') and isinstance(m, blacklist_weight_modules):
+                    # 黑名单中的参数不需要权重衰减
+                    no_decay.add(fpn)
+
+        # 注意：`transformer.wte.weight`和`lm_head.weight`分别出现在no_day和decay集中。
+        # 然而，在我们的实现中它们是绑定的，
+        # 由于`named_parameters()`不会返回重复项，因此它只会返回第一个出现项，
+        # 因此，让我们手动从衰减集decay中删除`lm_head.weight`。
+        decay.remove('lm_head.weight')
+
+        # 验证每个参数属于且只属于decay/no_decay中的一个
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        inter_params = decay & no_decay
+        union_params = decay | no_decay
+        assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
+        assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
+                                                    % (str(param_dict.keys() - union_params), )
+
+        # 创建PyTorch优化器
+        optim_groups = [
+            {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": weight_decay},
+            {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+        ]
+        # fused选项可以使AdamW优化器的速度更快
+        use_fused = (device_type == 'cuda') and ('fused' in inspect.signature(torch.optim.AdamW).parameters)
+        print(f"using fused AdamW: {use_fused}")
+        extra_args = dict(fused=True) if use_fused else dict()
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
+
+        return optimizer
 
     def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
         '''
--- models/gpt_impl/main.py
+++ models/gpt_impl/main.py
@@ -1,17 +1,29 @@
 import torch
-from gpt import GPT, GPTConfig
+import tiktoken
+from gpt import GPT
 
-config = GPTConfig()
-gpt_model = GPT(config)
+n_samples = 10
+max_new_tokens = 50
+top_k = 16
+temperature = 0.8
+prompt = 'I want to earn more money, so I'
+device = 'cpu'
+
+gpt_model = GPT.from_pretrained('gpt2', dict(dropout=0.0))
 gpt_model.eval()
+gpt_model.to(device)
 
+eos_token = '<|endoftext|>'
+enc = tiktoken.get_encoding('gpt2')
+encode = lambda s: enc.encode(s, allowed_special={eos_token})
+decode = lambda l: enc.decode(l)
 
-sequence = [2, 3, 11, 4]
-inp = torch.tensor(sequence).unsqueeze(dim=0)
-print(inp.shape)
-out = gpt_model.generate(inp, 20)
-print(out[0].shape)
-
-
+start_ids = encode(prompt)
+x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
+with torch.no_grad():
+    for k in range(n_samples):
+        y = gpt_model.generate(x, max_new_tokens, temperature, top_k)
+        print(decode(y[0].tolist()))
+        print('-' * 100)
 
 
